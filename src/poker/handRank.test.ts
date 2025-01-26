9ca442e8e865e6dc6cab84a0f37d0319052a0e70
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2025-01-26 10:02:24 +0700

    Add hand rank

--- src/poker/handRank.test.ts
+++ src/poker/handRank.test.ts
@@ -1,6 +1,6 @@
-import { describe, test, expect } from "bun:test";
-import { Rank, HandRank } from "./handRank";
-import { GameResult } from "./hand";
+import { describe, test, expect, it } from "bun:test";
+import { Rank, HandRank, handRankFromHand, CardFrequency } from "./handRank";
+import { Card, GameResult, Hand, Suits } from "./hand";
 
 const allRanks = [
   Rank.Nothing,
@@ -234,4 +234,211 @@ describe("Hand rank comparison", () => {
       });
     }
   );
+});
+
+describe("hand rank from hand", () => {
+  test("Can determined Straight Flush", () => {
+    const straighFlushCards = [
+      new Card(14, Suits.Club),
+      new Card(13, Suits.Club),
+      new Card(12, Suits.Club),
+      new Card(10, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+    const cards = straighFlushCards;
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.StraightFlush);
+    expect(result.getHighs()).toEqual([14, 13, 12, 11, 10]);
+  });
+
+  test("Can determined Four of a kind", () => {
+    const straighFlushCards = [
+      new Card(4, Suits.Club),
+      new Card(4, Suits.Club),
+      new Card(4, Suits.Club),
+      new Card(4, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+    const cards = straighFlushCards;
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.FourOfAKind);
+    expect(result.getHighs()).toEqual([4, 11]);
+  });
+
+  test("Can determined two pair", () => {
+    const cards = [
+      new Card(10, Suits.Club),
+      new Card(10, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(12, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.TwoPairs);
+    expect(result.getHighs()).toEqual([11, 10, 12]);
+  });
+
+  test("Can determined three of a kind", () => {
+    const cards = [
+      new Card(10, Suits.Club),
+      new Card(12, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.ThreeOfAKind);
+    expect(result.getHighs()).toEqual([11, 12, 10]);
+  });
+
+  test("Can determined straight", () => {
+    const cards = [
+      new Card(10, Suits.Club),
+      new Card(11, Suits.Diamond),
+      new Card(12, Suits.Club),
+      new Card(13, Suits.Club),
+      new Card(14, Suits.Club),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.Straight);
+    expect(result.getHighs()).toEqual([14, 13, 12, 11, 10]);
+  });
+
+  test("Can determined straight ace first", () => {
+    const cards = [
+      new Card(14, Suits.Club),
+      new Card(2, Suits.Diamond),
+      new Card(3, Suits.Club),
+      new Card(4, Suits.Club),
+      new Card(5, Suits.Club),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.Straight);
+    expect(result.getHighs()).toEqual([5, 4, 3, 2, 1]);
+  });
+
+  test("Can determined flush", () => {
+    const cards = [
+      new Card(14, Suits.Club),
+      new Card(2, Suits.Club),
+      new Card(5, Suits.Club),
+      new Card(3, Suits.Club),
+      new Card(10, Suits.Club),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.Flush);
+    expect(result.getHighs()).toEqual([14, 10, 5, 3, 2]);
+  });
+
+  test("Can determined Full-house", () => {
+    const cards = [
+      new Card(14, Suits.Club),
+      new Card(14, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Diamond),
+      new Card(11, Suits.Heart),
+    ];
+    const hand = new Hand(cards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.FullHouse);
+    expect(result.getHighs()).toEqual([11, 14]);
+  });
+
+  test("Can determined no pair", () => {
+    const noPairCards = [
+      new Card(10, Suits.Club),
+      new Card(5, Suits.Diamond),
+      new Card(14, Suits.Club),
+      new Card(12, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+    const hand = new Hand(noPairCards);
+    const result = handRankFromHand(hand);
+    expect(result.getRank()).toEqual(Rank.Nothing);
+    expect(result.getHighs()).toEqual([14, 12, 11, 10, 5]);
+  });
+});
+
+describe("Card frequencies", () => {
+  test("Can calculate frequency and card values", () => {
+    const card1 = [
+      new Card(14, Suits.Club),
+      new Card(14, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Diamond),
+      new Card(11, Suits.Heart),
+    ];
+    const card2 = [
+      new Card(14, Suits.Club),
+      new Card(2, Suits.Club),
+      new Card(5, Suits.Club),
+      new Card(3, Suits.Club),
+      new Card(10, Suits.Club),
+    ];
+
+    const card3 = [
+      new Card(10, Suits.Club),
+      new Card(12, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+
+    const result1 = new CardFrequency(card1);
+    expect(result1.cardFrequencySorted()).toEqual([3, 2]);
+    expect(result1.cardValueSortedByFrequency()).toEqual([11, 14]);
+
+    const result2 = new CardFrequency(card2);
+    expect(result2.cardFrequencySorted()).toEqual([1, 1, 1, 1, 1]);
+    expect(result2.cardValueSortedByFrequency()).toEqual([14, 10, 5, 3, 2]);
+
+    const result3 = new CardFrequency(card3);
+    expect(result3.cardFrequencySorted()).toEqual([3, 1, 1]);
+    expect(result3.cardValueSortedByFrequency()).toEqual([11, 12, 10]);
+  });
+
+  test("Can match frequency pattern", () => {
+    const card1 = [
+      new Card(14, Suits.Club),
+      new Card(14, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Diamond),
+      new Card(11, Suits.Heart),
+    ];
+    const card2 = [
+      new Card(14, Suits.Club),
+      new Card(2, Suits.Club),
+      new Card(5, Suits.Club),
+      new Card(3, Suits.Club),
+      new Card(10, Suits.Club),
+    ];
+
+    const card3 = [
+      new Card(10, Suits.Club),
+      new Card(12, Suits.Diamond),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+      new Card(11, Suits.Club),
+    ];
+
+    const result1 = new CardFrequency(card1);
+    const result2 = new CardFrequency(card2);
+    const result3 = new CardFrequency(card3);
+
+    expect(result1.isFrequencyMatch([3, 2])).toBeTruthy();
+    expect(result1.isFrequencyMatch([3, 1, 1])).toBeFalsy();
+
+    expect(result2.isFrequencyMatch([1, 1, 1, 1, 1])).toBeTruthy();
+    expect(result2.isFrequencyMatch([2, 1, 1, 1])).toBeFalsy();
+
+    expect(result3.isFrequencyMatch([3, 1, 1])).toBeTruthy();
+    expect(result3.isFrequencyMatch([4, 1])).toBeFalsy();
+  });
 });
--- src/poker/handRank.ts
+++ src/poker/handRank.ts
@@ -1,4 +1,4 @@
-import { GameResult, Hand } from "./hand";
+import { Card, GameResult, Hand } from "./hand";
 
 export enum Rank {
   Nothing = 0,
@@ -12,6 +12,149 @@ export enum Rank {
   StraightFlush = 8,
 }
 
+function isConsecutive(cardSorted: Card[]): boolean {
+  for (let index = 1; index < cardSorted.length; index++) {
+    if (cardSorted[index - 1].number !== cardSorted[index].number + 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isFlush(cardSorted: Card[]): boolean {
+  const suits = cardSorted[0].suits;
+  for (const card of cardSorted) {
+    if (card.suits !== suits) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function arrayValueEquals(arr1: number[], arr2: number[]): boolean {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+export function handRankFromHand(hand: Hand): HandRank {
+  let cardSorted = hand.cards.sort((a, b) => b.number - a.number);
+
+  const cardNumbers = [];
+  for (const card of cardSorted) {
+    cardNumbers.push(card.number);
+  }
+  // Special case for straight ace first
+  if (arrayValueEquals(cardNumbers, [14, 5, 4, 3, 2])) {
+    cardSorted[0].number = 1;
+    // Reshuffle, put ace last
+    cardSorted = [
+      cardSorted[1],
+      cardSorted[2],
+      cardSorted[3],
+      cardSorted[4],
+      cardSorted[0],
+    ];
+  }
+  const consequtive = isConsecutive(cardSorted);
+  const flush = isFlush(cardSorted);
+  const cardFrequency = new CardFrequency(hand.cards);
+  if (consequtive && flush) {
+    return new HandRank(
+      Rank.StraightFlush,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (cardFrequency.isFrequencyMatch([4, 1])) {
+    return new HandRank(
+      Rank.FourOfAKind,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (cardFrequency.isFrequencyMatch([3, 2])) {
+    return new HandRank(
+      Rank.FullHouse,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (flush) {
+    return new HandRank(Rank.Flush, cardFrequency.cardValueSortedByFrequency());
+  }
+  if (consequtive) {
+    return new HandRank(
+      Rank.Straight,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (cardFrequency.isFrequencyMatch([3, 1, 1])) {
+    return new HandRank(
+      Rank.ThreeOfAKind,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (cardFrequency.isFrequencyMatch([2, 2, 1])) {
+    return new HandRank(
+      Rank.TwoPairs,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  if (cardFrequency.isFrequencyMatch([2, 1, 1, 1])) {
+    return new HandRank(
+      Rank.TwoPairs,
+      cardFrequency.cardValueSortedByFrequency()
+    );
+  }
+  return new HandRank(Rank.Nothing, cardNumbers);
+}
+
+type FrequencyMap = { [key: number]: number };
+
+export class CardFrequency {
+  private cardFrequencyMap: FrequencyMap;
+  constructor(cards: Card[]) {
+    const cardFrequencyMap: FrequencyMap = {};
+    for (const card of cards) {
+      cardFrequencyMap[card.number] = (cardFrequencyMap[card.number] || 0) + 1;
+    }
+    this.cardFrequencyMap = cardFrequencyMap;
+  }
+
+  cardFrequencySorted(): number[] {
+    return Object.values(this.cardFrequencyMap).sort().reverse();
+  }
+
+  private getCardValues() {
+    return Object.keys(this.cardFrequencyMap).map((a) => parseInt(a));
+  }
+
+  cardValueSortedByFrequency(): number[] {
+    return this.getCardValues().sort((b, a) => {
+      if (this.cardFrequencyMap[a] < this.cardFrequencyMap[b]) return -1;
+      if (this.cardFrequencyMap[a] > this.cardFrequencyMap[b]) return 1;
+      if (a < b) return -1;
+      if (b > a) return 1;
+      return 0;
+    });
+  }
+
+  isFrequencyMatch(dest: number[]): boolean {
+    const src = this.cardFrequencySorted();
+    if (src.length !== dest.length) {
+      return false;
+    }
+    for (let i = 0; i < src.length; i++) {
+      if (src[i] !== dest[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 export class HandRank {
   private rank: Rank;
   private highs: number[];
