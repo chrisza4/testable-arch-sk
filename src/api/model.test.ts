d3deb1ee7ef3c25f926e3caabaee02ea50996de8
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2025-02-08 10:22:27 +0700

    Add proper model test

--- src/api/model.test.ts
+++ src/api/model.test.ts
@@ -2,6 +2,7 @@ import { describe, test, expect, mock } from "bun:test";
 import { HandComparison } from "./model";
 import { Database } from "bun:sqlite";
 import { HandData } from "./data";
+import { Card, GameResult, Hand, Suits } from "../poker/hand";
 
 describe("HandComparison", () => {
   test("Should throw Hand not found when hand does not exists in data", () => {
@@ -14,5 +15,88 @@ describe("HandComparison", () => {
     expect(() => subject.compare("123", "456")).toThrowError(
       "Cannot find hand"
     );
+  });
+
+  test("Should throw Hand not found when hand is not valid", () => {
+    const handData = new HandData(new Database(":memory:"));
+    handData.getHandById = mock(() => {
+      return new Hand([new Card(1, Suits.Club)]);
+    });
+
+    const subject = new HandComparison(handData);
+    expect(() => subject.compare("123", "456")).toThrowError(
+      "Cannot find hand"
+    );
+  });
+
+  test("Should return result of comparison when win", () => {
+    // We can also mock hand comparison by creating another hand comparison service as well
+    // This is when we should design what level of integration do we have
+    // In this case, I think it make sense to let both hand model and hand comparison module break
+    // If hand comparison logic failed, let both break
+
+    const handData = new HandData(new Database(":memory:"));
+    handData.getHandById = mock((handId) => {
+      if (handId === "123") {
+        return new Hand([
+          new Card(13, Suits.Heart),
+          new Card(12, Suits.Diamond),
+          new Card(13, Suits.Club),
+          new Card(13, Suits.Spade),
+          new Card(13, Suits.Heart),
+        ]);
+      }
+      return new Hand([
+        new Card(13, Suits.Heart),
+        new Card(12, Suits.Diamond),
+        new Card(13, Suits.Club),
+        new Card(12, Suits.Spade),
+        new Card(13, Suits.Heart),
+      ]);
+    });
+
+    const subject = new HandComparison(handData);
+    expect(subject.compare("123", "456")).toEqual(GameResult.Win);
+  });
+
+  test("Should return result of comparison when lose", () => {
+    const handData = new HandData(new Database(":memory:"));
+    handData.getHandById = mock((handId) => {
+      if (handId === "123") {
+        return new Hand([
+          new Card(13, Suits.Heart),
+          new Card(12, Suits.Diamond),
+          new Card(13, Suits.Club),
+          new Card(13, Suits.Spade),
+          new Card(13, Suits.Heart),
+        ]);
+      }
+      return new Hand([
+        new Card(13, Suits.Heart),
+        new Card(12, Suits.Diamond),
+        new Card(13, Suits.Club),
+        new Card(12, Suits.Spade),
+        new Card(13, Suits.Heart),
+      ]);
+    });
+
+    const subject = new HandComparison(handData);
+    expect(subject.compare("456", "123")).toEqual(GameResult.Lose);
+  });
+
+  test("Should return result of comparison when draw", () => {
+    const handData = new HandData(new Database(":memory:"));
+    handData.getHandById = mock((handId) => {
+      return new Hand([
+        new Card(13, Suits.Heart),
+        new Card(12, Suits.Diamond),
+        new Card(13, Suits.Club),
+        new Card(13, Suits.Spade),
+        new Card(13, Suits.Heart),
+      ]);
+    });
+
+    const subject = new HandComparison(handData);
+    expect(subject.compare("456", "123")).toEqual(GameResult.Draw);
   });
 });
--- src/api/model.ts
+++ src/api/model.ts
@@ -1,4 +1,4 @@
-import type { GameResult } from "../poker/hand";
+import type { GameResult, Hand } from "../poker/hand";
 import { HandData } from "./data";
 import { Database } from "bun:sqlite";
 
@@ -9,6 +9,16 @@ export class HandComparison {
   }
 
   compare(handId: string, anotherHandId: string): GameResult {
-    throw new Error("Not implemented yet");
+    let hand: Hand, anotherHand: Hand;
+    try {
+      hand = this.handData.getHandById(handId);
+      anotherHand = this.handData.getHandById(anotherHandId);
+    } catch (err) {
+      throw new Error("Cannot find hand");
+    }
+    if (!hand.validate() || !anotherHand.validate()) {
+      throw new Error("Cannot find hand");
+    }
+    return hand.compare(anotherHand);
   }
 }
