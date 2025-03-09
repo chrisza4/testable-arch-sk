eec31d4c6beac4d656c8c8dc97519d939d8f38ea
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2025-03-09 09:55:03 +0700

    Add dotnet sample

---
+++ csharp/Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace Test;
+
+using Moq;
+
+public class UnitTest1
+{
+    public class Subject(UnitTest1.Dependency dependency)
+    {
+        private readonly Dependency dependency = dependency;
+
+        public int MyMethod()
+        {
+            return this.dependency.GetValueFromExternalSystem() + 30;
+        }
+    }
+
+    public class Dependency
+    {
+        public virtual int GetValueFromExternalSystem()
+        {
+            return 5;
+        }
+    }
+    [Fact]
+    public void Test()
+    {
+        var mockDependency = new Mock<Dependency>();
+        mockDependency.Setup(m => m.GetValueFromExternalSystem()).Returns(10);
+        var subject = new Subject(mockDependency.Object);
+        Assert.Equal(40, subject.MyMethod());
+    }
+}
